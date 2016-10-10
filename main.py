5ac294baeff73e40c922572b8a537a06078ceeae
Dmitry Shpetnyi <shpetnyi@g5software.com>
2016-10-10 19:20:19 +0300

    started dummy text responsing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram.ext import *
 import chatterbot
 import logging
+import re
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -14,8 +15,38 @@ def hello(bot, update):
         'Hello {}'.format(update.message.from_user.first_name)
     )
 
+def process_text(text): # todo rename and debidlo
+    numbers = re.findall(r'\b\d+[а-я]?\b', text)
+    
+    if (!numbers)
+      return ""
+
+    for number in numbers:
+        answer = ""
+        if (not number[len(number) - 1].isdigit()):
+            if (number[len(number) - 1] == 'и'):
+              answer += "Корпус И "
+            elif (number[len(number) - 1] == 'з'):
+              answer += "Корпус З "
+            else:
+              answer += "Главный корпус "  
+        else:
+            answer += "Главный корпус "
+        
+        answer += "Этаж " + number[0]
+        
+        return answer
+
+
+
+def auditory(bot, update):
+    update.message.reply_text(
+          "DSOKDOSK"
+      )
+
 def message_handler(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text=chat_bot.get_response(update.message.text).text)
+    bot.sendMessage(chat_id=update.message.chat_id, text=process_text(update.message.text))
 
 chat_bot = chatterbot.ChatBot(
       "NUREbot", 
@@ -34,6 +65,7 @@ updater = Updater('259933822:AAGoMk2Fb2YwBP6bOMl69a4E7DDmXBrxtz4')
 updater.dispatcher.add_handler(MessageHandler([Filters.text], message_handler))
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('auditory', auditory))
 
 updater.start_polling()
 updater.idle()
