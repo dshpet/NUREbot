f76580b73d07aff85338b49a64c56957fd7f6d0b
Dmitry Shpetnyi <shpetnyi@g5software.com>
2016-10-21 18:55:35 +0300

    make project a vs2015 solution. Begin cist schedule parsing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from telegram.ext import *
 import chatterbot
 import logging
 import re
+import json
+import urllib
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -11,14 +13,20 @@ def start(bot, update):
     update.message.reply_text('Bot is now in English. A multi-language too')
 
 def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name)
-    )
+    update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
 
 def process_text(text): # todo rename and debidlo
+    aud_msg = parse_auditory_number(text)
+
+    url =  urllib.request.urlopen("http://cist.nure.ua/ias/app/tt/").read()
+    cist_schedule = json.loads(url)
+
+    return aud_msg
+
+def parse_auditory_number(text):
     numbers = re.findall(r'\b\d+[а-я]?\b', text)
     
-    if (!numbers)
+    if not numbers:
       return ""
 
     for number in numbers:
@@ -37,26 +45,31 @@ def process_text(text): # todo rename and debidlo
         
         return answer
 
-
-
 def auditory(bot, update):
-    update.message.reply_text(
-          "DSOKDOSK"
-      )
+    update.message.reply_text("DSOKDOSK")
 
 def message_handler(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text=chat_bot.get_response(update.message.text).text)
     bot.sendMessage(chat_id=update.message.chat_id, text=process_text(update.message.text))
 
-chat_bot = chatterbot.ChatBot(
-      "NUREbot", 
-      storage_adapter="chatterbot.adapters.storage.JsonFileStorageAdapter",
-      logic_adapters=[
-        "chatterbot.adapters.logic.MathematicalEvaluation",
+
+cist_api_root = "http://cist.nure.ua/ias/app/tt"
+faculties_url =  urllib.request.urlopen(cist_api_root + "/get_faculties").read()
+faculties = json.loads(faculties_url.decode('UTF-8'))
+
+groups_url = cist_api_root + "/get_groups?faculty_id=95" # 95 = CS fac
+groups_json = urllib.request.urlopen(groups_url).read().decode('UTF-8')
+groups = json.loads(groups_json)
+
+group_url = cist_api_root + "/get_schedule?group_id=5742300" # 5742300 PZSm-16-1
+group_json = urllib.request.urlopen(group_url).read().decode('UTF-8')
+group_sched = json.loads(group_json)
+
+chat_bot = chatterbot.ChatBot("NUREbot", 
+      storage_adapter = "chatterbot.adapters.storage.JsonFileStorageAdapter",
+      logic_adapters = ["chatterbot.adapters.logic.MathematicalEvaluation",
         "chatterbot.adapters.logic.TimeLogicAdapter",
-        "chatterbot.adapters.logic.ClosestMatchAdapter",
-      ]
-    )
+        "chatterbot.adapters.logic.ClosestMatchAdapter",])
 chat_bot.set_trainer(chatterbot.trainers.ChatterBotCorpusTrainer)
 chat_bot.train("chatterbot.corpus.english")
 
